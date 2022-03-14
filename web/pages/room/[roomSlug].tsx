2292a77e99cec83eda3c497ce1fbb522501c936d
Blayne Marjama <blayne.marjama@gmail.com>
2022-03-13 19:54:41 -0700

    make ui slightly prettier

--- web/pages/room/[roomSlug].tsx
+++ web/pages/room/[roomSlug].tsx
@@ -18,9 +18,9 @@ const DisplayEntrant = ({ id, name }: PlayerType) => (
 )
 
 const DisplayPrizes = ({ firstPlace, secondPlace, thirdPlace, kills, altSplit, totalPrize, totalEntrants }: PrizeValuesType & { totalEntrants: number }) => (
-  <div className="border-2 border-slate-100 rounded">
+  <div>
     <h3 className="font-medium leading-tight text-xl text-center mt-0 mb-2">Prize Split</h3>
-    <ul className="bg-white rounded-lg w-96 text-gray-900">
+    <ul className="border-2 border-slate-100 roundedbg-white rounded-lg w-96 text-gray-900">
       <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">Total Entrants: {totalEntrants} Bird Warriors</li>
       <li className="px-6 py-2 border-b border-gray-200 w-full">Kills: {kills} sFNC</li>
       <li className="px-6 py-2 border-b border-gray-200 w-full">1st: {firstPlace} sFNC</li>
