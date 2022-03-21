37e4332aaa653ee072e175c6eeaa5e5549f96830
Blayne Marjama <blayne.marjama@gmail.com>
2022-03-21 15:32:00 -0700

    removed activities from server, added example to rumble app

--- server/src/helpers/createRumble.ts
+++ server/src/helpers/createRumble.ts
@@ -1,6 +1,5 @@
 import RumbleApp, { ActivitiesObjType, PlayerType, PrizeSplitType } from "@rumble-raffle-dao/rumble";
 
-// TODO: Fetch activities from database.
 export const createGame = async (
   activities: ActivitiesObjType,
   prizeSplit: PrizeSplitType,
