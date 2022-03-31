6c8560f41593de91b2a755c83413842e9837c35e
Blayne Marjama <blayne.marjama@gmail.com>
2022-03-30 22:20:47 -0700

    now will highlight player any time they are involved in an activity

--- web/components/room/activityLog.tsx
+++ web/components/room/activityLog.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'react-popper-tooltip/dist/styles.css';
-import { RoundActivityLog, SingleActivity } from "@rumble-raffle-dao/types";
+import { PickFromPlayers, RoundActivityLog, SingleActivity, SupabaseUserType } from "@rumble-raffle-dao/types";
 import { ClickToCopyPopper } from '../Popper';
 
 const replaceActivityDescPlaceholders = (activity: SingleActivity): (string | JSX.Element)[] => {
   const matchPlayerNumber = /(PLAYER_\d+)/ // matches PLAYER_0, PLAYER_12, etc
   const parts = activity.description.split(matchPlayerNumber);
+  console.log(activity);
 
   const replaceNames = parts.map((part, i) => {
     if (part.match(matchPlayerNumber)) {
@@ -19,13 +20,19 @@ const replaceActivityDescPlaceholders = (activity: SingleActivity): (string | JS
   return replaceNames
 }
 
-const DisplayActivityLog = (logs: RoundActivityLog) => {
+
+const DisplayActivityLog = ({logs, user}: { logs: RoundActivityLog; user: SupabaseUserType; }) => {
+  /**
+   * Returns true if the play is present in the array
+   */
+  const containsUser = (participants: PickFromPlayers[]) => participants.findIndex(p => p.public_address === user.public_address) > -1;
+
   return (
     <div>
       <h3>Round {logs.round_counter + 1}</h3>
       <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
         {logs.activities?.map((activity, index) => (
-          <li className="px-6 py-2 border-b border-gray-200 w-full" key={`${activity.id}-${index}`}>
+          <li className={`px-6 py-2 border-b border-gray-200 ${ containsUser(activity.participants) ? 'bg-slate-200' : 'bg-white'} w-full`} key={`${activity.id}-${index}`}>
             {replaceActivityDescPlaceholders(activity)}
           </li>
         ))}
