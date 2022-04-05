1691faa63f8ec80d273bca340f7d137344378387
Blayne Marjama <blayne.marjama@gmail.com>
2022-04-04 23:55:38 -0700

    forgot timer in dark mode

--- web/pages/room/[roomSlug].tsx
+++ web/pages/room/[roomSlug].tsx
@@ -211,7 +211,7 @@ const RumbleRoom = ({ activeRoom, roomCreator, roomSlug, ...rest }: ServerSidePr
           </div>
           {/* Left Side */}
           <div className="pr-6 lg:pr-20 md:pr-6 sm:pr-6 py-2 flex-1 overflow-auto scrollbar-thin dark:scrollbar-thumb-rumbleSecondary scrollbar-thumb-rumblePrimary scrollbar-track-rumbleBgDark" style={{ height: calcHeight }}>
-            <div className="my-4 h-6 text-center">
+            <div className="my-4 h-6 text-center dark:text-rumbleNone text-rumbleOutline">
               {timeToGameStart && <span>Game starts in: {timeToGameStart}</span>}
               {timeToNextRoundStart && <span>Next round begins in: {timeToNextRoundStart}</span>}
             </div>
