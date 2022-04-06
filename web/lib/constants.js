a22c0b19fdda8c45a8e8648f251e6942ae3de028
Blayne Marjama <blayne.marjama@gmail.com>
2022-04-05 18:15:01 -0700

    update urls

--- web/lib/constants.js
+++ web/lib/constants.js
@@ -4,8 +4,8 @@ if (process.env.NODE_ENV === 'development') {
   BASE_API_URL = 'http://localhost:3001';
   BASE_WEB_URL = 'http://localhost:3000'
 } else {
-  BASE_API_URL = 'PROD'
-  BASE_WEB_URL = 'prod'
+  BASE_API_URL = 'https://rumble-raffle-dao.herokuapp.com'
+  BASE_WEB_URL = 'https://rumble-raffle-dao-iota.vercel.app'
 }
 
 export {BASE_API_URL, BASE_WEB_URL};
