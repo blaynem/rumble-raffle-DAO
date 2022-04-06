26a78ab2f8d6b16d6bae55650e0053391835d583
Blayne Marjama <blayne.marjama@gmail.com>
2022-04-05 18:29:58 -0700

    wrong url

--- server/constants.js
+++ server/constants.js
@@ -2,7 +2,7 @@ let BASE_WEB_URL;
 if (process.env.NODE_ENV === 'development') {
   BASE_WEB_URL = 'http://localhost:3000'
 } else {
-  BASE_WEB_URL = 'https://rumble-raffle-dao.herokuapp.com'
+  BASE_WEB_URL = 'https://rumble-raffle-1ey8jwut1-rumble-raffle-dao.vercel.app/'
 }
 
 export {BASE_WEB_URL};
