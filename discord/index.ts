a9d98a2b303d54be1fb6563c8a643fe1811dd5e1
Blayne Marjama <blayne.marjama@gmail.com>
2022-07-19 00:23:18 -0700

    split out discord api calls

--- discord/index.ts
+++ discord/index.ts
@@ -1,18 +1,19 @@
 require('dotenv').config()
 import http from "http";
 import fetch from 'node-fetch';
-import { JOIN_GAME, PATH_UNLINK_DISCORD, PATH_VERIFY_INIT, SERVER_AUTH_DISCORD, SERVER_BASE_PATH, SERVER_ROOMS, SERVER_USERS } from '@rumble-raffle-dao/types/constants';
-import { fetchPlayerRoomData, initSockets, JOIN_GAME_BUTTON_ID, socket, UNLINK_DISCORD_BUTTON_ID } from "./sockets";
+import { SERVER_BASE_PATH, SERVER_ROOMS } from '@rumble-raffle-dao/types/constants';
+import { fetchPlayerRoomData, initSockets, JOIN_GAME_BUTTON_ID, UNLINK_DISCORD_BUTTON_ID } from "./sockets";
 import client from './client';
 import { BASE_API_URL } from './constants';
 import { interactionCommands } from './deploy-commands';
-import { ButtonInteraction, CacheType, MessageActionRow, MessageButton, MessageEmbed } from 'discord.js';
-import { AuthDiscordInitBody, AuthDiscordInitPostResponse, AuthDiscordVerifyPostResponse, CreateRoom, UserDataFetchByDiscordId } from '@rumble-raffle-dao/types';
+import { MessageActionRow, MessageButton } from 'discord.js';
+import { CreateRoom } from '@rumble-raffle-dao/types';
 import { CONFIG } from './config';
+import { onJoinGamePressed, onUnlinkDiscord } from "./api";
 
 const token = process.env.DISCORD_TOKEN
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 0;
 
 http.createServer().listen(port)
 
@@ -182,99 +183,6 @@ client.on('interactionCreate', async interaction => {
     })
   }
 });
-
-/**
- * Does the Discord Auth verification fetch
- */
-const fetchVerifyInit = async (fetchBody: AuthDiscordInitBody) => {
-  return await fetch(`${BASE_API_URL}${SERVER_BASE_PATH}${SERVER_AUTH_DISCORD}${PATH_VERIFY_INIT}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(fetchBody)
-  }).then(res => res.json()) as AuthDiscordInitPostResponse
-}
-
-/**
- * When the user presses the "Join Game" button, this function will fire.
- * 
- * @param interaction - Discords ButtonInteraction type
- */
-const onJoinGamePressed = async (interaction: ButtonInteraction<CacheType>) => {
-  const fetchBody: AuthDiscordInitBody = {
-    discord_id: interaction.user.id,
-    discord_tag: `${interaction.user.username}#${interaction.user.discriminator}`
-  };
-
-  // Check if the users discord_id is attached to a users public_address.
-  const userFetch: UserDataFetchByDiscordId = await fetch(`${BASE_API_URL}${SERVER_BASE_PATH}${SERVER_USERS}?discord_id=${fetchBody.discord_id}`)
-    .then(res => res.json());
-
-  // If the user fetch comes back with a discord_id
-  if (userFetch?.discord_id) {
-    // join the game through the discord sockets.
-    socket.emit(JOIN_GAME, userFetch, CONFIG.roomSlug);
-    // Return a reply that we joined the game.
-    await interaction.reply({
-      ephemeral: true,
-      content: 'You have joined the game.'
-    });
-    return;
-  }
-
-  // Otherwise we start the verification init
-  const { data: verifyInitData, error } = await fetchVerifyInit(fetchBody);
-
-  if (error) {
-    await interaction.reply({
-      content: 'There was an error when attempting to join the game, please let the devs know.',
-      ephemeral: true,
-    });
-  }
-
-  // todo: Check if they are linked, if so we should either: 1. join the game for them or 2. tell them "already joined"
-  const embed = new MessageEmbed()
-    .setColor('#4CE3B6')
-    .setTitle('Visit Rumble Raffle site to verify!')
-    .setDescription(`
-The easiest way to play Rumble Raffle through Discord is by clicking the button below and linking your account. This allows a single button press to join game in the future!
-
-If you would prefer to not link your account, you can join via the site instead [RumbleRaffle.com](${CONFIG.gameUrl}).
-
-***Important: Do not share this link.*** *It is unique to your discord_id*
-`)
-
-  // Now we set the link
-  const linkButton = new MessageButton()
-    .setLabel('Link Discord')
-    .setURL(verifyInitData.verify_link)
-    .setStyle('LINK');
-
-  const row = new MessageActionRow()
-    .addComponents(linkButton);
-
-  await interaction.reply({
-    embeds: [embed],
-    ephemeral: true,
-    components: [row]
-  });
-}
-
-const onUnlinkDiscord = async (interaction: ButtonInteraction<CacheType>) => {
-  const { data, error } = await fetch(`${BASE_API_URL}${SERVER_BASE_PATH}${SERVER_AUTH_DISCORD}${PATH_UNLINK_DISCORD}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ discord_id: interaction.user.id })
-  }).then(res => res.json()) as AuthDiscordVerifyPostResponse
-  if (error) {
-    interaction.update({ content: 'Please let the admins know there was an error.', components: [] });
-    return;
-  }
-  interaction.update({ content: data, components: [] });
-}
 
 // Login to Discord with your client's token
 client.login(token);
